b363697f09cd09c2cf1f716c33e363db2a773ebd
masayayoshida <m.yoshida5544@gmail.com>
2016-04-12 23:19:01 +0000

    add favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,6 +4,7 @@ class Micropost < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140 }
   
   has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
   
   def favorited_by? user
     favorites.where(user_id: user.id).exists?
